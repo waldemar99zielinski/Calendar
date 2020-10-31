87bb8ef68a97ada565f5c7ef7a08cc91af3c4182
Waldemar <waldek99zielinski@gmail.com>
2020-10-31 18:28:31 +0100

    TBL strategy

--- t.pl
+++ t.pl
@@ -61,20 +61,7 @@ sub getArrayEventsFromNotes{
 
     return @sorted_events;
 }
-#print $events[0]->getDate();
-=put
-my @sorted_events = sort {$a->{date}->strftime("%Y.%m.%d") cmp $b->{date}->strftime("%Y.%m.%d")} @events;
 
-foreach my $event (@events){
-    print $event->getDate()->strftime("%Y.%m.%d");
-    print "\n";
-} 
-print "\n";
-foreach (@sorted_events){
-    print $_->getDate()->strftime("%Y.%m.%d");
-    print "\n";
-}
-=cut
 =put
 my @dates;
 #print $events[0]->getDate();
@@ -82,7 +69,7 @@ my @dates;
 #my $date = $events[0]->getDate() + ONE_DAY;
 print "Insert begining date(yyyy.mm.dd): ";
 my $date = <STDIN>;
-my $xd = chomp($date);
+chomp($date);
 #print "$date";
 my $check = "2013.10.23";
 
@@ -148,7 +135,7 @@ sub getArrayOfDatesForMonths{
 
 my @events = getArrayEventsFromNotes();
 #my @dates = getArrayOfDates("2013.02.07", 6);
-my @dates = getArrayOfDatesForMonths("2020.10.15", 4);
+my @dates = getArrayOfDatesForMonths("2020.10.15", 3);
 my @calendarDays;
 
 #arguments (\dates, \events) passing arrays as a reference
@@ -211,11 +198,135 @@ foreach my $day (@calendarDays){
        print "\n";
 } 
 =cut
-
+=put
 open(FH, '>', "cal.tex") or die $!;
 
 print FH getTeXDocument(\@calendarDays);
 
 close(FH);
 #print(getTeXDocument(\@calendarDays));
+=cut
 
+sub getTBLDay{
+    my $dayOfTheMonth = $_[0];
+    my @events = @{$_[1]};
+    my $eventsText = "";
+    #TeX formated day
+
+    #add date
+    
+    #add events
+    foreach my $event (@events){
+   
+        $eventsText = $eventsText."\n\n-$event"; 
+    }
+        my $day=
+"T{
+$dayOfTheMonth $eventsText
+T}";
+    return $day;
+}
+#my @a = ("XD", "dwa bardzo dlugie informacje");
+#print(getTBLDay(1, \@a));
+
+#arguments(@caledarDaysArray), should contain dates for only one month
+sub getTBLTable{
+    my @caledarDaysArray = @{$_[0]};
+    my $month = $caledarDaysArray[0]->getDate()->fullmonth." ".$caledarDaysArray[0]->getDate()->year;
+    my $week = "";
+    my $empytDay = " &";
+    my $currentDayOfTheWeek = 0;
+    #align first date 
+    my $dayOfTheWeekForTheFirstDay = $caledarDaysArray[0]->getDate()->_wday;
+    for(my $i = 0; $i<$dayOfTheWeekForTheFirstDay; $i++){
+     
+        $week = $week.$empytDay;
+        #print ("alignment: ",$currentDayOfTheWeek, " ", $week, "\n");
+        $currentDayOfTheWeek++;
+        
+    }
+
+    foreach my $day(@caledarDaysArray){
+        
+        if($currentDayOfTheWeek<6){
+          
+            my @descArray = $day->getEventsDesc();
+            $week = $week.getTBLDay($day->getDate()->mday, \@descArray);
+            $week = $week."&";
+            #print ("week: ",$currentDayOfTheWeek, " ", $week, "\n");
+            $currentDayOfTheWeek++;
+            
+        }else{#end table line
+            
+            my @descArray = $day->getEventsDesc();
+            $week = $week.getTBLDay($day->getDate()->mday, \@descArray);
+            $week = $week."\n";
+            #print ("endweek: ",$currentDayOfTheWeek, " ", $week, "\n");
+            $currentDayOfTheWeek = 0;
+        }
+    }
+    
+    if($currentDayOfTheWeek != 0){
+        #pupulate table with empy cells
+        while($currentDayOfTheWeek<6){
+            $week = $week.$empytDay;
+            $currentDayOfTheWeek++;
+        }    
+       
+    }else{
+        #delete last new line
+        $week = substr($week, 0, (length $week)-1);
+    }
+    
+    
+    
+    my $table = 
+".TS
+center allbox tab(&);
+cB s s s s s s
+cw(0.7i)B cw(0.7i)B cw(0.7i)B cw(0.7i)B cw(0.7i)B cw(0.7i)B cw(0.7i)B
+lw(0.7i) lw(0.7i) lw(0.7i) lw(0.7i) lw(0.7i) lw(0.7i) lw(0.7i).
+$month
+Sun & Mon & Tue & Wed & Thu & Fri & Sat
+$week
+.TE
+";
+    return $table;
+}
+#arguments(@cacaledarDaysArray)
+sub getTBLDocument{
+    my @caledarDaysArray = @{$_[0]};
+    my $tables = "";
+    my $currentMonth = $caledarDaysArray[0]->getDate()->mon;
+
+    my @calendarDaysForOneMonth;
+
+    foreach my $day(@caledarDaysArray){
+     
+        if($currentMonth == $day->getDate()->mon){
+            push(@calendarDaysForOneMonth, $day);
+         
+            #print($day->getDate(), "\n");
+        }else{
+            #
+            $tables = $tables.getTBLTable(\@calendarDaysForOneMonth)."\n";
+            @calendarDaysForOneMonth = ();
+            push(@calendarDaysForOneMonth, $day);
+            $currentMonth = $day->getDate()->mon;
+            #print($day->getDate(), "\n");
+        }
+       
+       
+    } 
+
+    $tables = $tables.getTBLTable(\@calendarDaysForOneMonth);
+
+    
+   
+    return $tables;
+}
+open(FH, '>', "test.roff") or die $!;
+
+print FH getTBLDocument(\@calendarDays);
+
+close(FH);
